a8a1541da263313931f4e795c0e59176b6bf9eea
Larkin22 <larkinjw@gmail.com>
2017-03-29 21:23:40 -0400

    Push Text Processing Solutions

---
+++ Text Processing/Sort Command #3.sh
@@ -0,0 +1,12 @@
+#You are given a text file where each line contains a number.
+#The numbers may be either an integer or have decimal places.
+#There will be no extra characters other than the number or the
+#newline at the end of each line. Sort the lines in ascending order
+#- so that the first line holds the numerically smallest number,
+#and the last line holds the numerically largest number.
+
+#The -n option sorts the file on the basis of the numeric fields available
+#if the first word or column in the file is a number.
+
+
+sort -n
