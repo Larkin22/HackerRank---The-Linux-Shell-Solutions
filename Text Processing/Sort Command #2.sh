f0820824b9b5a417dbe0e84fe159f7f2542b1959
Larkin22 <larkinjw@gmail.com>
2017-03-29 21:18:41 -0400

    Push Text Processing Solutions

--- Text Processing/Sort Command #2.sh
+++ Text Processing/Sort Command #2.sh
@@ -1,4 +1,7 @@
 Given a text file, order the lines in reverse lexicographical order
 (i.e. Z-A instead of A-Z).
 
+The -r option reverses the sorting order to either the reverse of the usual
+lexicographical ordering or descending order while sorting in numerical mode.
+
 sort -r
